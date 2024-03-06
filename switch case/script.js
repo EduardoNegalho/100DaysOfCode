ab764bc4fc0a082de8215ad31d2e69579430f4e6
Eduardo Marques <eduardomar1703@gmail.com>
2024-03-05 22:14:21 -0300

    estudos sobre switch case

---
+++ switch case/script.js
@@ -0,0 +1,13 @@
+switch ("b") {
+    case 'a':
+        console.log("Resultado é a")
+        break
+    case("b"):
+        console.log("Resultado é b")
+        break;
+    case("c"):
+        console.log("Resultado é c")
+        break;
+    default:
+        console.log("Finalizando...")
+}
